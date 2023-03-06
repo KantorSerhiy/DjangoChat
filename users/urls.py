b737e4a7093f421b19c5e833d215f640f0908cab
skantor <serhiykantor@gmail.com>
2023-03-06 20:13:16 +0200

    add simple register & login v1

--- users/urls.py
+++ users/urls.py
@@ -1,1 +1,10 @@
-urlpatterns = []
+from django.urls import path
+from django.contrib.auth import views as auth_views
+from .views import RegisterView, index
+
+urlpatterns = [
+    path('login/', auth_views.LoginView.as_view(redirect_authenticated_user=True), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('register/', RegisterView.as_view(), name='register'),
+]
+
--- users/views.py
+++ users/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.contrib.auth import login
+from django.shortcuts import redirect, render
+from django.views import View
 
-# Create your views here.
+from users.forms import CustomUserForm
+
+
+class RegisterView(View):
+    def get(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect('users:index')
+
+        form = CustomUserForm()
+        return render(request, 'registration/register.html', {
+            'form': form
+        })
+
+    def post(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect('/')
+
+        form = CustomUserForm(request.POST or None)
+        if form.is_valid():
+            # Model Form Returns the object created
+            user = form.save()
+            login(request, user)
+            return redirect('')
+
+        return render(request, 'registration/register.html', {
+            'form': form
+        })
+
+def index(request):
+    return render(request, "index.html")
